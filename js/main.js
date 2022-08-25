8a3729a78de9e590db7be518c5dcd4deed67b136
Annushka1997 <annushkach1997@mail.ru>
2022-08-25 20:30:15 +0400

    Film App with some changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 "use strict";
 const headerImage = document.querySelector("header img");
-const adv = document.querySelectorAll("#main_promo .mp");
 const filmsBlock = document.querySelector("#films");
 const form = document.querySelector("#add");
+const signInBtn = document.querySelector("[data-in]");
+const signInModal = document.querySelector("[data-in-modal]");
+const toggleButton = document.querySelector(".toggle_button");
+const navBarLinks = document.querySelector("ul");
 
-headerImage.src = "img/bg2.jpg";
-headerImage.alt = "Hitman's Wife's Bodyguard";
-adv.forEach(adv => adv.remove());
-document.title = headerImage.alt;
+function init () {
+	headerImage.src = "img/bg2.jpg";
+	
+	if (headerImage.src.slice(27) === "img/bg1.jpg") {
+		headerImage.alt = "Gemini Man";
+	} else if (headerImage.src.slice(27) === "img/bg2.jpg") {
+		headerImage.alt = "Hitman's Wife's Bodyguard";
+	}
 
+	document.querySelectorAll("#main_promo .mp").forEach(adv => adv.remove());
+	document.title = headerImage.alt;
+}
+init();
 const _DB = {
 	movies: [
 		"Logan", "Spider-Man", "The Seven Samurai",
@@ -16,23 +27,19 @@ const _DB = {
 		"The Deer Hunter", "Rocky", "Crid"
 	]
 };
-let newD;
-const favoriteDB =  [];
-const check = document.querySelector('input[type="checkbox"]');
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	let val = e.target.firstElementChild.value.trim();
-	let valCopy = val;
-	if (val !== "" && _DB.movies.indexOf(val) == -1) {
+	const check = document.querySelector('input[type="checkbox"]');
+
+	if (val !== "" && val.charAt(0) !== "<" && val.charAt(0) !== ">" && val.charAt(0) !== "/" && _DB.movies.indexOf(val) === -1) {
 		if (check.checked) {
-			favoriteDB.push(val);
-			_DB.movies.push(val);
-			console.log(`this film <${valCopy}> added to favorite`);
-		} else {
-			_DB.movies.push(val);
+			console.log(`this film <${val}> added to favorite`);
 		}
+		_DB.movies.push(val);
 	}
+
 	setSort(_DB.movies);
 	createFilmsList(_DB.movies, filmsBlock);
 	e.target.reset();
@@ -44,69 +51,43 @@ function setSort (arr) {
 function createFilmsList (films, parent) {
 	parent.innerHTML = "";
 	setSort(films);
-		films.forEach((film, index) => { if (index < 20 ) {
-		if (favoriteDB.indexOf(film) !== -1) {
-		parent.innerHTML += `
-			<p> 
-			<font color="blue">  
-			${index + 1}. ${film.length >= 21 ? `${film.slice(0, 21)}...` : film}
-			</font> 
-				<span data-rm>&#128465;</span>
-			</p>
-		`;
-	} else {
-		parent.innerHTML += `
-			<p> 
-			${index + 1}. ${film.length >= 21 ? `${film.slice(0, 21)}...` : film}
-				<span data-rm>&#128465;</span>
-			</p>
-		`;
-	}
-	} else {
-		let randomFilm = function (arr) {
-			let newArray;
-			for (let i = 20; i>0; i--) {
-				let tmp = arr[i];
-				let rnd = Math.floor(Math.random() * (i+1));
-				arr[i] = arr [rnd];
-				arr[rnd] = tmp;
-				newArray = arr;
-				console.log(newArray);
-			}
-			parent.innerHTML = "";
-			newArray.forEach((film,index) => { if (index < 20) {
-				if (favoriteDB.indexOf(film) !== -1) {
-					parent.innerHTML += `
-						<p> 
-						<font color="blue">  
-						${index + 1}. ${film.length >= 21 ? `${film.slice(0, 21)}...` : film}
-						</font> 
-							<span data-rm>&#128465;</span>
-						</p>
-					`;
-				} else {
-					parent.innerHTML += `
-						<p> 
-						${index + 1}. ${film.length >= 21 ? `${film.slice(0, 21)}...` : film}
-							<span data-rm>&#128465;</span>
-						</p>
-					`;
-				}
-			}
-			});
-	};
-	randomFilm(_DB.movies);
-	}
-	});
 
-	document.querySelectorAll("[data-rm]").forEach((btn, i) => {
+	films.forEach((film, index) => {
+		parent.innerHTML += `
+			<p>
+				${index + 1}. ${film.length >= 21 ? film.slice(0, 21)+'...' : film} 
+				<span data-rm>&#128465;</span>
+			</p>
+		`;
+	});
+	removeFilmFromList("[data-rm]");
+}
+function removeFilmFromList(selector) {
+	setSort(_DB.movies);
+	document.querySelectorAll(selector).forEach((btn, i) => {
 		btn.addEventListener("click", () => {
 			btn.parentElement.remove();
 			_DB.movies.splice(i, 1);
-			createFilmsList(films, parent);
+			createFilmsList(_DB.movies, filmsBlock);
 		});
 	});
 }
+
 createFilmsList(_DB.movies, filmsBlock);
 
+// Առաջին հարցի պատասխանը
+signInBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	signInModal.classList.add("modal_wrapper-active");
+});
 
+window.addEventListener("mousedown",(e) => {
+	if (e.target !== signInModal.querySelector("form") && e.target.tagName !== "INPUT") {
+		signInModal.classList.remove("modal_wrapper-active");
+	}
+});
+
+// Երկրորդ հարցի պատասխանը 
+toggleButton.addEventListener("click", () => {
+	navBarLinks.classList.toggle('active');
+});
